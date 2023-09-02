cc68b749bf985fb9e9794f2a65a50d43b2789e75
Yczar <babalolagbogo@gmail.com>
2023-09-02 21:52:49 +0100

    chore: create initial brache cli package

---
+++ packages/brache_cli/lib/brache_cli.dart
@@ -0,0 +1,10 @@
+/// brache_cli, My new Dart CLI package
+///
+/// ```sh
+/// # activate brache_cli
+/// dart pub global activate brache_cli
+///
+/// # see usage
+/// brache --help
+/// ```
+library brache_cli;
