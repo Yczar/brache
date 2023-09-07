05b0934396b7602013afe41f613d2742e734d16e
Yczar <babalolagbogo@gmail.com>
2023-09-07 01:17:48 +0100

    chore: export shapes component

---
+++ packages/brache/lib/src/components/src/shapes/shapes.dart
@@ -0,0 +1,7 @@
+export 'src/circle.dart';
+export 'src/ellipse.dart';
+export 'src/line.dart';
+export 'src/polygon.dart';
+export 'src/rectangle.dart';
+export 'src/square.dart';
+export 'src/triangle.dart';
