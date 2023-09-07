c09733e0b008a57ce197edc1b5abd30b8ae3f0fe
Yczar <babalolagbogo@gmail.com>
2023-09-07 01:05:37 +0100

    chore: add runApp function

--- packages/brache/lib/src/brache.dart
+++ packages/brache/lib/src/brache.dart
@@ -1,7 +1,17 @@
-/// {@template brache}
-/// My new Dart package
-/// {@endtemplate}
-class Brache {
-  /// {@macro brache}
-  const Brache();
+import 'package:brache/src/core/src/app/src/brache_app.dart';
+
+export 'components/components.dart';
+
+export 'core/core.dart';
+
+/// Bootstraps the given [Brache] application and initiates its build process.
+/// This function should be called at the entry point of the application to
+/// start the rendering and execution of the app.
+///
+/// [app] represents the main application instance derived from [Brache].
+void runApp(Brache app) {
+  app
+    ..setup()
+    ..build()
+    ..postRender();
 }
